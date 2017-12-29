208bbdb5dff45a2d64877d72e2279ba5a7ed3a32
ltw <ltw@126.com>
2017-12-29 15:52:16 +0800

    service修改提交

--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/TbItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/TbItemServiceImpl.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
 import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pageHelper.DataGridResult;
 import cn.e3mall.pojo.TbItem;
 import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.pojo.TbItemExample.Criteria;
@@ -36,6 +40,28 @@ public class TbItemServiceImpl implements TbItemService {
 			return list.get(0);
 		}
 		return null;
+	}
+
+	@Override
+	public DataGridResult getItemByDataGrid(int page, int rows) {
+		//1.设置分页
+		PageHelper.startPage(page, rows);
+		//执行查询
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = tbItemMapper.selectByExample(example );
+		//取分页信息
+		PageInfo<TbItem> info = new PageInfo<>(list);
+		
+		//获取总的条数
+		long total = info.getTotal();
+		
+		//创建DataGridResult对象
+		DataGridResult result = new DataGridResult();
+		//为DataGridResult对象设置参数
+		result.setTotal(total);
+		result.setRows(list);
+		//返回result
+		return result;
 	}
 
 }
