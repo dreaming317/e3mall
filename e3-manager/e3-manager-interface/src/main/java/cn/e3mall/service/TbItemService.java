1cadaada65cb0fea46962efc9a0291f0c8dbee08
ltw <ltw@126.com>
2017-12-29 15:54:45 +0800

    添加获取商品列表接口及实例化

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/TbItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/TbItemService.java
@@ -1,8 +1,11 @@
 package cn.e3mall.service;
 
+import cn.e3mall.pageHelper.DataGridResult;
 import cn.e3mall.pojo.TbItem;
 
 public interface TbItemService {
 
 	TbItem getItemById(long itemId);
+	
+	DataGridResult getItemByDataGrid(int page,int rows);
 }
